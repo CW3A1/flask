0f3d2a7dc3b15e1dba2e073369509985d71c43af
Victor Broeders <victor.broeders@student.kuleuven.be>
2021-10-07 17:01:54 +0200

    Added new html templates and a css file + directory

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 from flask import Flask,render_template
 
-app = Flask(__name__, template_folder='HTML_Templates')
+app = Flask(__name__, template_folder='HTML_Templates',static_folder='Style')
 @app.route('/')
 def home():
     return render_template('home.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 if __name__ == '__main__':
     app.run()
