61267a53c872690041e674957f4612147d90c4cc
Victor Broeders <victor.broeders@student.kuleuven.be>
2021-10-11 14:52:41 +0200

    Added some extra things

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__, template_folder='HTML_Templates',static_folder='Style')
 
 # Defining different HTML pages with their corresponding app routes
+
 # Main page
 @app.route('/')
 def home():
@@ -15,10 +16,16 @@ def home():
 def about():
     return render_template('about.html')
 
-#Redirecting 'admin' page
+# Window with variable
+@app.route('/<variable>')
+def user(variable):
+    return f'<h1> Hello {variable} <h1>'
+
+#Redirecting 'admin' page via a window with fixed variable
 @app.route('/admin')
 def admin():
-    return redirect(url_for('home'))
+    return redirect(url_for('user',variable='Admin'))
+
 
 # Making sure the app runs on startup
 if __name__ == '__main__':
