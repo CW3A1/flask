89249b54b2ca7683c9131741c57ebd99fb6f1fe6
Victor Broeders <victor.broeders@student.kuleuven.be>
2021-10-11 17:21:48 +0200

    Final version of test website

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 # Importing Flask
-from flask import Flask, render_template, redirect, url_for, request,session
+from flask import Flask, render_template, redirect, url_for, request,session, flash
 from datetime import timedelta
+from blueprint import second
+
 # Defining different Flask-folders and names
+
 app = Flask(__name__, template_folder='HTML_Templates',static_folder='Style')
 app.secret_key = 'PNOISFUCKINGAWESOME'
 app.permanent_session_lifetime = timedelta(days=0,hours=0,minutes=5)
+
 # Defining different HTML pages with their corresponding app routes
 
 # Main page
@@ -26,27 +30,44 @@ def admin():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        session.permanent = True
-        user = request.form['naam']
+        session.permanent = False
+        user = request.form['username']
         session['user'] =user
+        flash('Login succesful!')
         return redirect(url_for('user'))
     else:
         if 'user' in session:
+            flash('Already logged In!')
             return redirect(url_for('user'))
         else:
+            flash('You are not logged in!')
             return render_template('login.html')
-@app.route('/user')
+@app.route('/user', methods=['POST','GET'])
 def user():
+    email = None
     if 'user' in session:
         user = session['user']
-        return f'<h1>{user}</h1>'
+        if request.method == 'POST':
+            email = request.form['email']
+            session['email'] = email
+        else:
+            if 'email' in session:
+                email = session['email']
+        return render_template('user.html', email = email)
     else:
         return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
+    if 'user' in session:
+        user = session['user']
+        flash('You have been logged out!', 'info')
     session.pop('user', None)
+    session.pop('email', None)
     return redirect(url_for('login'))
+
+# Trying multiple scripts using Blueprint
+
 # Making sure the app runs on startup
 if __name__ == '__main__':
     app.run(debug=True)
