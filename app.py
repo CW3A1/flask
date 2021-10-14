2b4515302ca07391b01c71daa98afd740c56d3d9
Mohammed Andiche <mohammed.andiche@student.kuleuven.be>
2021-10-14 22:24:55 +0200

    fuck this shit

--- app.py
+++ app.py
@@ -1,32 +1,24 @@
-# Importing Flask
-from flask import Flask, render_template, redirect, url_for, request,session, flash
+from flask import Flask, render_template, redirect, url_for, request,session, flash, jsonify
 from datetime import timedelta
-
-# Defining different Flask-folders and names
+import requests
 
 app = Flask(__name__, template_folder='HTML_Templates',static_folder='Style')
 app.secret_key = 'PNOISFUCKINGAWESOME'
 app.permanent_session_lifetime = timedelta(days=0,hours=0,minutes=5)
 TEMPLATES_AUTO_RELOAD = True
 
-# Defining different HTML pages with their corresponding app routes
-
-# Main page
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# About page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-#Redirecting 'admin' page via a window with fixed variable
 @app.route('/admin')
 def admin():
     return redirect(url_for('user',variable='Admin'))
 
-# Trying to make a functional button
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -42,6 +34,7 @@ def login():
         else:
             flash('You are not logged in!')
             return render_template('login.html')
+
 @app.route('/user', methods=['POST','GET'])
 def user():
     email = None
@@ -66,10 +59,11 @@ def logout():
     session.pop('email', None)
     return redirect(url_for('login'))
 
+@app.route('/api')
+def test():
+    resp = requests.get("http://localhost:11001/api/column/all")
+    print(resp.json())
+    return jsonify(resp.json())
 
-
-# Trying multiple scripts using Blueprint
-
-# Making sure the app runs on startup
 if __name__ == '__main__':
     app.run(debug=True)
