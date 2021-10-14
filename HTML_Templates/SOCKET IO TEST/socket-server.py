0ce8a9ec3c89f66cff06e421ffd1b668f3570272
Victor Broeders <victor.broeders@student.kuleuven.be>
2021-10-14 16:21:30 +0200

    Final version of test website secure version

--- HTML_Templates/SOCKET IO TEST/socket-server.py
+++ HTML_Templates/SOCKET IO TEST/socket-server.py
@@ -5,10 +5,18 @@ app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'PNONISFUCKINGAWESOME'
 socket = SocketIO(app)
 
-@app.route('/')
-def home():
+number1 = 0
+number2 = 0
+@app.route('/<n1>/<n2>')
+def home(n1,n2):
+    number1 = n1
+    number2 = n2
     return render_template('socket-site.html')
 
+@socket.on('number')
+def hanedmessage(msg):
+    print('Message:' + msg)
+    send(msg, broadcast=True)
 
 
 
